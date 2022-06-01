1e6c8b66ad71dbc01145547119ebd0c2fef76bae
Júlio César Oliveira <juliocomp99@hotmail.com>
2022-06-01 16:26:26 -0300

    Mais dados API rick-and-morty

--- rick-and-morty/src/App.js
+++ rick-and-morty/src/App.js
@@ -2,24 +2,53 @@ import "./App.css";
 import { useEffect, useState } from "react";
 import Chacarcters from "./components/Chacarcters";
 import Logo from "./Logo/Logo.png";
+import Run from "./Run/Run.png";
 
 function App() {
   const [characterList, setCharacterList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character/")
+    fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`)
       .then((response) => response.json())
       .then((response) => setCharacterList(response.results))
       .catch((err) => console.log(err));
-  }, []);
+  }, [currentPage]);
 
-  console.log(characterList);
+  const previousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const nextPage = () => {
+    if (currentPage < 42) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
 
   return (
     <div>
       <header className="App-header">
         <img className="image" alt="logo" src={Logo} />
       </header>
+
+      <div className="button__pages">
+        <div className="button__image">
+          <img className="image__run" alt="run" src={Run} />
+          <button className="button__pages--back--next" onClick={previousPage}>
+            Voltar
+          </button>
+        </div>
+       <h2 className="page">{currentPage}</h2>
+        <div className="button__image">
+          <img id="inverter" className="image__run" alt="run" src={Run} />
+          <button className="button__pages--back--next" onClick={nextPage}>
+            Avançar
+          </button>
+        </div>
+      </div>
       <div className="container">
         {characterList.length > 0 && (
           <Chacarcters characterList={characterList} />
--- rick-and-morty/src/components/CharCard/index.js
+++ rick-and-morty/src/components/CharCard/index.js
@@ -3,8 +3,24 @@ import "./style.css";
 function CharCard({ person }) {
   return (
     <div className="container__person">
-      <h2>{person.name}</h2>
+      <h2 className="name">{person.name}</h2>
       <img className="img__card" alt={person.name} src={person.image} />
+
+      <div className="element__container">
+        <p className="element">
+          <strong>Espécie:</strong> {person.species}
+        </p>
+        <p className="element">
+          <strong>Origem:</strong> {person.origin.name}
+        </p>
+        <p className="element">
+          <strong>Localidade:</strong> {person.location.name}
+        </p>
+        <p className="element">
+          <strong>Sexo:</strong> {person.gender}
+        </p>
+      </div>
+
       <div>
         {person.status === "Alive" ? (
           <h3 className="status__vivo">O personagem está vivo</h3>
